337bd3b70311aa170917645becd38ada77f0fb26
Ali Coruk <70493706+something8701@users.noreply.github.com>
2024-09-23 14:09:13 +1000

    This is the base of my app im not sure what i m going to do but yeah

---
+++ MacroHit/MacrohitApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct MacrohitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark) // Enforcing dark mode
+        }
+    }
+}
